ffee11d4b26a4615c833e8b669851cdbff91d6df
spotlesscoder <codingspiderfox@gmail.com>
2024-09-18 09:30:52 +0000

    feat: add sample test

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,1 +1,13 @@
-pub fn foo() {}
+pub fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_works() {
+        assert_eq!(2, add(1, 1));
+    }
+}
